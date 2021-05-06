36de5a9134293f0f2e3d9111cd7080890ba48d8f
Raphael Okeibunor <raphaelnagato19@gmail.com>
2021-05-06 17:37:27 +0100

    Add Custom Value Resolver for Product picture url

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -10,7 +10,8 @@ namespace API.Helpers
         {
             CreateMap<Product, ProductToReturnDto>()
             .ForMember(p => p.ProductBrand, o => o.MapFrom(p => p.ProductBrand.Name))
-            .ForMember(p => p.ProductType, o => o.MapFrom(p => p.ProductType.Name));
+            .ForMember(p => p.ProductType, o => o.MapFrom(p => p.ProductType.Name))
+            .ForMember(p => p.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
         }
     }
 }
