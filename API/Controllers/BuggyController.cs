4b7a3f725b54f62796a94e19b91fed7aaf49c5ae
Raphael Okeibunor <raphaelnagato19@gmail.com>
2021-05-07 02:34:47 +0100

    create base controller and and buggy controller

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,36 @@
+using API.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            return NotFound(new ApiResponse(404));
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            string thing = null;
+            var thingToReturn = thing.ToString();
+            return Ok();
+        }
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetBadRequestValidation(int id)
+        {
+            return NotFound();
+        }
+
+    }
+
+    
+}
